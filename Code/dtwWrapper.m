84c2a8efc03385e07d7c36d29173edb945648c13
Hari Maruthachalam <srihari.m.srihari@gmail.com>
2018-10-17 10:52:38 +0530

    Save path fix

--- Code/dtwWrapper.m
+++ Code/dtwWrapper.m
@@ -28,8 +28,10 @@ else
 end
 imagesc(R);
 
-savefig(strcat(fullfile(savePath,DTWType),datestr(clock,'YYYY/mm/dd HH:MM:SS:FFF'),'.fig'));
-saveas(gcf,strcat(fullfile(savePath,DTWType),datestr(clock,'YYYY/mm/dd HH:MM:SS:FFF'),'.png'));
+mkdir(strcat(fullfile(savePath,DTWType), refLabel(index(1),:)));
+
+savefig(strcat(fullfile(savePath,DTWType, refLabel(index(1),:)), '/', datestr(clock,'mm_dd_HH_MM_SS_FFF'),'.fig'));
+saveas(gcf,strcat(fullfile(savePath,DTWType, refLabel(index(1),:)), '/', datestr(clock,'mm_dd_HH_MM_SS_FFF'),'.png'));
 close;
 
 retLabel = refLabel(index(1 : topC),:);
